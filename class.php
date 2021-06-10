0ececd536dec1242b89e9fbe0c8910ae6134c1e2
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-06-10 22:13:44 +0000

    Ajout de la class panier avec la fonction add() pour ajouter les produits dans la panier

--- class.php
+++ class.php
@@ -1,5 +1,7 @@
 <?php 
 require "maClassDB.php";
 $DB = new DB();
+require "panierClass.php";
+$panier = new panier($DB);
 
 ?>
---
+++ panierClass.php
@@ -0,0 +1,30 @@
+<?php 
+
+class panier{
+
+	private $DB;
+
+	public function __construct() {
+
+		if (!isset($_SESSION)) {
+			session_start();
+		}
+
+		if (!isset($_SESSION['panier'])) {
+			$_SESSION['panier'] = array();
+		}
+
+		this->DB = $DB;
+
+	}
+
+	public function add($produit_id) {
+		if (isset($_SESSION['panier'][$produit_id])) {
+			$_SESSION['panier'][$produit_id]++;
+		}else{
+			$_SESSION['panier'][$produit_id] = 1;
+		}
+	}
+}
+
+?>
