ffd1971dbf250c100b950658ccb84bb4f00fefd5
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-06-12 06:05:06 +0000

    Ajout du fichier addpanier.php

---
+++ addpanier.php
@@ -0,0 +1,20 @@
+<?php
+// require "header.php";
+require "mesClass.php";
+$json = array('error' => true);
+if(isset($_GET['id'])){
+    $product = $DB->query('SELECT id FROM produits WHERE id = :id', array('id'  => $_GET['id']));
+    if(empty($product)){
+        $json['message'] = "Ce produit n'éxiste pas !";
+    }
+    $panier->add($product[0]->id);
+    $json['error'] = false;
+    $json['total'] = $panier->total();
+    $json['taille'] = $panier->count();
+    $json['message'] = 'Le produit a été bien ajouté !';
+}else{
+    $json['message'] = "Vous n'avez pas ajouter de produit au panier !";
+}
+echo json_encode($json);
+?>
+<?php //require "footer.php"; ?> 
